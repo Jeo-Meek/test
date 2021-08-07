928527cb3ad149f31a2c0e804efd132d5235c219
Jeo-Meek <nebulameek@gmail.com>
2021-08-07 18:42:36 +0800

    apikifa

---
+++ app/Http/Controllers/PostsManController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsManController extends Controller
+{
+    public function store()
+    {
+        return "homepage";
+    }
+
+}
